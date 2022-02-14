8fd21eb1bb76d8085df0efb20f26f4c5e3fcaf29
frastsytt <frastsytt@gmail.com>
2022-02-14 09:33:47 +0200

    display the articles

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const mysql = require('mysql')
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
+const hbs = require('express-handlebars')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.engine('hbs', hbs.engine({
+	extname: 'hbs',
+	defaultLayout: 'main',
+	layoutsDir: __dirname+'/views/layouts/'
+}))
+
+app.use(express.static('public'))
+
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
@@ -15,6 +26,18 @@ var con = mysql.createConnection({
 con.connect(function(err) {
 	if (err) throw err;
 	console.log(`Connected to joga_mysql`)
+})
+
+app.get('/', (req, res) => {
+	let query ="SELECT * FROM article"
+	let articles = []
+	con.query(query, (err, result) => {
+		if(err) throw err;
+		articles = result
+		res.render('index', {
+			articles: articles
+		})
+	})
 })
 
 app.listen(3000, () => {
