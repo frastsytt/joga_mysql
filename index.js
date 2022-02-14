9d25eb4b38f32472525a9dd0f715201ede28369d
frastsytt <frastsytt@gmail.com>
2022-02-14 09:57:52 +0200

    display article by slug

--- index.js
+++ index.js
@@ -29,13 +29,26 @@ con.connect(function(err) {
 })
 
 app.get('/', (req, res) => {
-	let query ="SELECT * FROM article"
-	let articles = []
+	let query =`SELECT * FROM article`
+	let articles
 	con.query(query, (err, result) => {
 		if(err) throw err;
 		articles = result
 		res.render('index', {
 			articles: articles
+		})
+	})
+})
+
+app.get('/article/:slug', (req, res) => {
+	let query =`SELECT * FROM article WHERE slug="${req.params.slug}"`
+	let article
+	con.query(query, (err, result) => {
+		if(err) throw err;
+		article = result
+		console.log(article)
+		res.render('article', {
+			article: article
 		})
 	})
 })
